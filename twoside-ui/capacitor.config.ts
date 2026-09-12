9e4284cf621850f92d776ba289b5dc8560481218
Goshen009 <goshen009@gmail.com>
2026-09-13 00:20:09 +0100

    checkpoint

---
+++ twoside-ui/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dev.twoside.app',
+  appName: 'twoside',
+  webDir: 'dist'
+};
+
+export default config;
